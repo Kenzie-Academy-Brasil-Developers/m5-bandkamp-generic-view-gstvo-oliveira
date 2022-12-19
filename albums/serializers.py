9b9d94a567d8ae08954d3a09ee32523cb194d553
Gustavo Oliveira <gstvo.dev@gmail.com>
2022-12-19 15:52:17 -0300

    feat: tarefas 2 e 3 finalizadas

--- albums/serializers.py
+++ albums/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Album
 
 
-class AlbumSerializer(serializers.Serializer):
+class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
         fields = ['id', 'name', 'year', 'user_id']
--- songs/serializers.py
+++ songs/serializers.py
@@ -3,9 +3,11 @@ from rest_framework import serializers
 from .models import Song
 
 
-class SongSerializer(serializers.Serializer):
+class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = ['id', 'title', 'duration', 'album_id']
 
         read_only_fields = ['id','album_id']
+
+        
